ac95ff8f3a332534260bfff81070bff39b83aa88
kmahboob <kmahboob@securitycompass.com>
2022-04-25 11:49:01 -0400

    fixing root response

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/',(req,res) => {
-  console.log('gettting root');
+  res.send('gettting root');
 })
 
 app.post('/signin',(req,res) => {handleSignin(req,res,db)})
