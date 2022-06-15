4c616497f72f5b472f47194b3aba25c6816434a3
Khizer22 <khizer405@gmail.com>
2022-06-15 09:36:06 -0400

    file upload API progress

--- controllers/image.js
+++ controllers/image.js
@@ -36,7 +36,7 @@ const handelApiCall = (req,res,detection_type) => {
     // Check if fileupload
     // if (req.body.type === 'fileUpload'){
     console.log("BANANAS");
-    console.log(req);
+    console.log(req.body);
         // const imageBytes = fs.readFileSync(req.body.file);
         // mySource = {base64: imageBytes};
     // }
